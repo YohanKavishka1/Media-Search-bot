9031b864e5385375299f1f1c29cc01e1f411a8be
YohanKavishka1 <90844876+YohanKavishka1@users.noreply.github.com>
2021-11-08 21:35:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,7 +67,7 @@ async def start(bot, cmd):
                 buttons = [
                     [
                         InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+                        InlineKeyboardButton('Channel', url='https://t.me/joinchat/Q7ZMIBzBYCYxNzc1')
                     ]
                     ]
                 await bot.send_cached_media(
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("Join Our Group", url="https://t.me/joinchat/avd6pZaXFoRlMzg1")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
